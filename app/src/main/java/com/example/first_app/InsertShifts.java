f796ec33830a021fba37914940281d3ac9ab8a11
בן אפריימוב <ben.a254@gmail.com>
2020-06-23 23:34:53 +0300

    new update mercedes

--- app/src/main/java/com/example/first_app/InsertShifts.java
+++ app/src/main/java/com/example/first_app/InsertShifts.java
@@ -3,6 +3,7 @@ package com.example.first_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,14 @@ public class InsertShifts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_shifts);
+
+        findViewById(R.id.btnViewShift).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InsertShifts.this,ViewShifts.class));
+            }
+        });
+
         mAuth = FirebaseAuth.getInstance();
         myRef = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getUid()).child("Shifts");
 
@@ -37,11 +46,12 @@ public class InsertShifts extends AppCompatActivity {
         endTime = findViewById(R.id.endTime);
         addShift = findViewById(R.id.addShift);
 
-        shift = "start: " + startTime.getText().toString() + "end: " + endTime.getText().toString();
+
 
         addShift.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                shift = "start: " + startTime.getText().toString() + " end: " + endTime.getText().toString() + "  ";
                 ValueEventListener valueEventListener = new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
--- app/src/main/java/com/example/first_app/ViewShifts.java
+++ app/src/main/java/com/example/first_app/ViewShifts.java
@@ -1,19 +1,43 @@
 package com.example.first_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ViewShifts extends AppCompatActivity {
 
+    ListView listView;
+    ArrayList<String> arrayList = new ArrayList<>();
+    FirebaseAuth mAuth;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_shifts);
+
+        listView = (ListView)findViewById(R.id.listView);
+
+        mAuth = FirebaseAuth.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getUid()).child("Shifts");
 
         findViewById(R.id.btnAddShift).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,11 +46,47 @@ public class ViewShifts extends AppCompatActivity {
             }
         });
 
-        ListView lv = findViewById(R.id.listView);
 
-        String foods[] = {"Egg","Pizza","Pasta"};
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.each_item_row,
-                R.id.item,foods);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.each_item_row,
+                R.id.item,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    String value = ds.getValue(String.class);
+                    arrayList.add(value);
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        myRef.addListenerForSingleValueEvent(valueEventListener);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(ViewShifts.this)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this item?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                arrayList.remove(position);
+                                myRef.setValue(arrayList);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
     }
 }
