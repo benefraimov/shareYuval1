00ac6fc51c4c564720237eb925a90bed8a8a5082
בן אפריימוב <ben.a254@gmail.com>
2020-07-06 15:15:36 +0300

    new

--- app/src/main/java/com/example/first_app/ProfileFragment.java
+++ app/src/main/java/com/example/first_app/ProfileFragment.java
@@ -3,6 +3,8 @@ package com.example.first_app;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,25 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        view.findViewById(R.id.userEmail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.replace(R.id.navHostFragment, new FragmentOne());
+                transaction.commit();
+            }
+        });
+        return  view;
     }
 }
+
+
+
+
+//    FragmentManager fm = getSupportFragmentManager();
+//    FragmentTransaction transaction = fm.beginTransaction();
+//    transaction.replace(R.id.navHostFragment, new Fragment());
+//    transaction.commit();
